8d37af1be75712a3a5c5ba940cab8f7a9c20da6e
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-04-26 16:57:06 -0300

    TypeScript component, dinamic

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
-
+export class BigCardComponent implements OnInit {
+  photoCover:string =""
+  cardTitle:string ="Novo titulo"
+  cardDescription:string =""
+  
+  constructor() { }
+  
+  ngOnInit(): void {
+    
+  }
 }
